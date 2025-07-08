0a23e05e003fe7e2666ddf7756ad2272507c5567
Rachidme007 <rachidlaayal2017@gmail.com>
2025-07-08 13:10:39 +0100

    THIS THE PATH OF FANG YUAN ITS HARD

--- exercise.py
+++ exercise.py
@@ -5,4 +5,4 @@ print(
     '"Hello, My Name IS %s  And Iam %s  Years Old and I Live in %s "'
     % (myName, myAge, myCountry)
 )
-print(type(myName), "\n", type(myCountry), "\n", type(myAge))
+print(type(myName), "\n", type(myCountry), "\n", type(myAge), "fuck")
